de56726688cef514d45dc19af3678b5debf31cc6
israel jeronimo <silvaij16@gmail.com>
2025-04-21 01:17:21 -0300

    fix: sumOfTensFilter.js

--- src/filters/basicFilters/sumOfTensFilter.js
+++ src/filters/basicFilters/sumOfTensFilter.js
@@ -1,3 +1,4 @@
+// filtra os jogos que tenham a soma de suas dezenas entre '170' e '220'.
 const sumOfTensFilter = (tens) => {
   let isAGodNumber = false
   const sumOfTens = tens.reduce((acc, value) => acc + value,0)
